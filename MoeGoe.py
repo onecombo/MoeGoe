1cfc966aac531a782dc8be11da75ced5f6c9136c
CjangCjengh <101577701+cjangcjengh@users.noreply.github.com>
2022-10-04 18:18:34 +0800

    support non utf-8 codec

--- MoeGoe.py
+++ MoeGoe.py
@@ -12,6 +12,14 @@ from mel_processing import spectrogram_torch
 
 from scipy.io.wavfile import write
 
+
+def ex_print(text, escape=False):
+    if escape:
+        print(text.encode('unicode_escape').decode())
+    else:
+        print(text)
+
+
 def get_text(text, hps, cleaned=False):
     if cleaned:
         text_norm = text_to_sequence(text, hps.symbols, [])
@@ -22,6 +30,7 @@ def get_text(text, hps, cleaned=False):
     text_norm = LongTensor(text_norm)
     return text_norm
 
+
 def ask_if_continue():
     while True:
         answer = input('Continue? (y/n): ')
@@ -30,10 +39,12 @@ def ask_if_continue():
         elif answer == 'n':
             sys.exit(0)
 
-def print_speakers(speakers):
+
+def print_speakers(speakers, escape=False):
     print('ID\tSpeaker')
     for id, name in enumerate(speakers):
-        print(str(id) + '\t' + name)
+        ex_print(str(id) + '\t' + name, escape)
+
 
 def get_speaker_id(message):
     speaker_id = input(message)
@@ -44,29 +55,37 @@ def get_speaker_id(message):
         sys.exit(1)
     return speaker_id
 
+
 def get_label_value(text, label, default, warning_name='value'):
-    value=re.search(rf'\[{label}=(.+?)\]',text)
+    value = re.search(rf'\[{label}=(.+?)\]', text)
     if value:
         try:
-            text=re.sub(rf'\[{label}=(.+?)\]','',text,1)
-            value=float(value.group(1))
+            text = re.sub(rf'\[{label}=(.+?)\]', '', text, 1)
+            value = float(value.group(1))
         except:
             print(f'Invalid {warning_name}!')
             sys.exit(1)
     else:
-        value=default
+        value = default
     return value, text
 
-def get_label(text,label):
+
+def get_label(text, label):
     if f'[{label}]' in text:
-        return True,text.replace(f'[{label}]','')
+        return True, text.replace(f'[{label}]', '')
     else:
-        return False,text
+        return False, text
+
 
 if __name__ == '__main__':
-    model = input('Path of a VITS model: ')
-    config = input('Path of a config file: ')
-    
+    if '--escape' in sys.argv:
+        escape = True
+    else:
+        escape = False
+
+    model = "epo/test.pth"  # input('Path of a VITS model: ')
+    config = "epo/config.json"  # input('Path of a config file: ')
+
     hps_ms = utils.get_hparams_from_file(config)
     n_speakers = hps_ms.data.n_speakers if 'n_speakers' in hps_ms.data.keys() else 0
     n_symbols = len(hps_ms.symbols) if 'symbols' in hps_ms.keys() else 0
@@ -81,26 +100,26 @@ if __name__ == '__main__':
         **hps_ms.model)
     _ = net_g_ms.eval()
     utils.load_checkpoint(model, net_g_ms)
-    
-    if n_symbols!=0:
+
+    if n_symbols != 0:
         while True:
-            choice = input('TTS or VC? (t/v):')
+            choice = "t"  # input('TTS or VC? (t/v):')
             if choice == 't':
                 text = input('Text to read: ')
-                if text=='[ADVANCED]':
+                if text == '[ADVANCED]':
                     text = input('Raw text:')
                     print('Cleaned text is:')
-                    print(_clean_text(text, hps_ms.data.text_cleaners))
+                    ex_print(_clean_text(text, hps_ms.data.text_cleaners), escape)
                     continue
-                
-                length_scale,text=get_label_value(text,'LENGTH',1,'length scale')
-                noise_scale,text=get_label_value(text,'NOISE',0.667,'noise scale')
-                noise_scale_w,text=get_label_value(text,'NOISEW',0.8,'deviation of noise')
-                cleaned,text=get_label(text,'CLEANED')
+
+                length_scale, text = get_label_value(text, 'LENGTH', 1, 'length scale')
+                noise_scale, text = get_label_value(text, 'NOISE', 0.667, 'noise scale')
+                noise_scale_w, text = get_label_value(text, 'NOISEW', 0.8, 'deviation of noise')
+                cleaned, text = get_label(text, 'CLEANED')
 
                 stn_tst = get_text(text, hps_ms, cleaned=cleaned)
-                
-                print_speakers(speakers)
+
+                print_speakers(speakers, escape)
                 speaker_id = get_speaker_id('Speaker ID: ')
                 out_path = input('Path to save: ')
 
@@ -108,13 +127,16 @@ if __name__ == '__main__':
                     x_tst = stn_tst.unsqueeze(0)
                     x_tst_lengths = LongTensor([stn_tst.size(0)])
                     sid = LongTensor([speaker_id])
-                    audio = net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale, noise_scale_w=noise_scale_w, length_scale=length_scale)[0][0,0].data.cpu().float().numpy()
+                    audio = \
+                        net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale,
+                                       noise_scale_w=noise_scale_w,
+                                       length_scale=length_scale)[0][0, 0].data.cpu().float().numpy()
                 write(out_path, hps_ms.data.sampling_rate, audio)
-                
+
                 print('Successfully saved!')
                 ask_if_continue()
-                
-                    
+
+
             elif choice == 'v':
                 audio_path = input('Path of an audio file to convert:\n')
                 print_speakers(speakers)
@@ -127,62 +149,68 @@ if __name__ == '__main__':
                 y = audio.unsqueeze(0)
 
                 spec = spectrogram_torch(y, hps_ms.data.filter_length,
-                    hps_ms.data.sampling_rate, hps_ms.data.hop_length, hps_ms.data.win_length,
-                    center=False)
+                                         hps_ms.data.sampling_rate, hps_ms.data.hop_length, hps_ms.data.win_length,
+                                         center=False)
                 spec_lengths = LongTensor([spec.size(-1)])
                 sid_src = LongTensor([originnal_id])
 
                 with no_grad():
                     sid_tgt = LongTensor([target_id])
-                    audio = net_g_ms.voice_conversion(spec, spec_lengths, sid_src=sid_src, sid_tgt=sid_tgt)[0][0,0].data.cpu().float().numpy()
+                    audio = net_g_ms.voice_conversion(spec, spec_lengths, sid_src=sid_src, sid_tgt=sid_tgt)[0][
+                        0, 0].data.cpu().float().numpy()
                 write(out_path, hps_ms.data.sampling_rate, audio)
-                        
+
                 print('Successfully saved!')
                 ask_if_continue()
     else:
         model = input('Path of a hubert-soft model: ')
         from hubert_model import hubert_soft
+
         hubert = hubert_soft(model)
 
         while True:
             audio_path = input('Path of an audio file to convert:\n')
             print_speakers(speakers)
-            
+
             import librosa
+
             if use_f0:
                 audio, sampling_rate = librosa.load(audio_path, sr=hps_ms.data.sampling_rate, mono=True)
                 audio16000 = librosa.resample(audio, orig_sr=sampling_rate, target_sr=16000)
             else:
                 audio16000, sampling_rate = librosa.load(audio_path, sr=16000, mono=True)
-            
+
             target_id = get_speaker_id('Target speaker ID: ')
             out_path = input('Path to save: ')
-            length_scale,out_path=get_label_value(out_path,'LENGTH',1,'length scale')
-            noise_scale,out_path=get_label_value(out_path,'NOISE',0.1,'noise scale')
-            noise_scale_w,out_path=get_label_value(out_path,'NOISEW',0.1,'deviation of noise')
-                
+            length_scale, out_path = get_label_value(out_path, 'LENGTH', 1, 'length scale')
+            noise_scale, out_path = get_label_value(out_path, 'NOISE', 0.1, 'noise scale')
+            noise_scale_w, out_path = get_label_value(out_path, 'NOISEW', 0.1, 'deviation of noise')
+
             from torch import inference_mode, FloatTensor
             import numpy as np
+
             with inference_mode():
                 units = hubert.units(FloatTensor(audio16000).unsqueeze(0).unsqueeze(0)).squeeze(0).numpy()
                 if use_f0:
-                    f0_scale,out_path = get_label_value(out_path,'F0',1,'f0 scale')
+                    f0_scale, out_path = get_label_value(out_path, 'F0', 1, 'f0 scale')
                     f0 = librosa.pyin(audio, sr=sampling_rate,
-                        fmin=librosa.note_to_hz('C0'),
-                        fmax=librosa.note_to_hz('C7'),
-                        frame_length=1780)[0]
+                                      fmin=librosa.note_to_hz('C0'),
+                                      fmax=librosa.note_to_hz('C7'),
+                                      frame_length=1780)[0]
                     target_length = len(units[:, 0])
-                    f0 = np.nan_to_num(np.interp(np.arange(0, len(f0)*target_length, len(f0))/target_length,
-                        np.arange(0, len(f0)), f0)) * f0_scale
+                    f0 = np.nan_to_num(np.interp(np.arange(0, len(f0) * target_length, len(f0)) / target_length,
+                                                 np.arange(0, len(f0)), f0)) * f0_scale
                     units[:, 0] = f0 / 10
-            
+
             stn_tst = FloatTensor(units)
             with no_grad():
                 x_tst = stn_tst.unsqueeze(0)
                 x_tst_lengths = LongTensor([stn_tst.size(0)])
                 sid = LongTensor([target_id])
-                audio = net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale, noise_scale_w=noise_scale_w, length_scale=length_scale)[0][0, 0].data.float().numpy()
+                audio = \
+                    net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale, noise_scale_w=noise_scale_w,
+                                   length_scale=length_scale)[0][0, 0].data.float().numpy()
             write(out_path, hps_ms.data.sampling_rate, audio)
-            
+
             print('Successfully saved!')
             ask_if_continue()
